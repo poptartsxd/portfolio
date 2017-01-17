ac90ed390cd8523aa3f67fad2f7633a282d48be8
poptartsxd <victoria.j.chai@gmail.com>
2017-01-17 11:15:15 -0500

    portfolio update

--- js/script.js
+++ js/script.js
@@ -2,5 +2,20 @@ $(document).ready(function(){
 
 	$(".scrollspy").scrollSpy();
 	$(".button-collapse").sideNav();
+
+
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() >= 300) { 
+	        $('#goToTop').fadeIn(500); 
+	    } else {
+	        $('#goToTop').fadeOut(500); 
+	    }
+
+	});
+	$('#goToTop').click(function() {
+	    $('body').animate({
+	        scrollTop : 0                
+	    }, 500);
+	});
 	
 });
