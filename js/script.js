3a2e9be25609a435fcb0dd2bf53feb92f16dc32e
poptartsxd <victoria.j.chai@gmail.com>
2017-01-19 12:34:05 -0500

    added side nav bar

--- js/script.js
+++ js/script.js
@@ -2,7 +2,30 @@ $(document).ready(function(){
 
 	$(".scrollspy").scrollSpy();
 	$(".button-collapse").sideNav();
+	$(".myProcessNav").pushpin({
+		top:700,
+		offset:100
+	});
 
+	$(".medMinderProcessNav").pushpin({
+		top:1000,
+		offset:100
+	});
+
+	$(".argonProcessNav").pushpin({
+		top:1000,
+		offset:100
+	});
+
+	$(".lockitProcessNav").pushpin({
+		top:800,
+		offset:100
+	});
+
+	$(".connectProcessNav").pushpin({
+		top:1000,
+		offset:100
+	});
 
 	$(window).scroll(function() {
 	    if ($(this).scrollTop() >= 300) { 
